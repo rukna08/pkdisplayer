bda0880f880d8ca9ad1f8bdd0f492d39a1163cce
Ankur Banerjee <rukna2003@gmail.com>
2022-11-16 18:23:39 +0530

    -

--- main.h
+++ main.h
@@ -8,6 +8,8 @@ void SetWindowClass(WNDCLASSA WindowClass, HINSTANCE Instance);
 
 void ProcessMessage();
 
+LRESULT CALLBACK KBDHook(int NCode, WPARAM WParam, LPARAM LParam);
+
 int g_WindowWidth = 150;
 
 int g_WindowHeight = 150;
@@ -21,3 +23,7 @@ char* g_Text = "NULL";
 UINT_PTR IDT_TIMER;
 
 int g_WindowWaitTime = 1;
+
+HHOOK g_KeyboardHookHandle;
+
+LPARAM g_LParam;
